c215a032c16934b43c66d8cde84be6eb88ded705
Ella Ksenofontova <ellaksenofontova@gmail.com>
2024-03-15 21:45:50 +0300

    Исправила баги, в т.ч. связанные с адаптивностью

--- src/StylePage/StyleCard.js
+++ src/StylePage/StyleCard.js
@@ -2,7 +2,7 @@ export default function StyleCard({styleName, styleOrder}) {
     const styleTitle = styleOrder == "before" ? `${styleName} стиль` : "Стиль " + `${styleName.toLowerCase()}`
 
     return (
-        <div className="style-image" style={{background: `url("Materials/Images/${styleName}.jpg") center / cover, url("Materials/Images/${styleName}.svg") center / cover, #F3DAB3 center / cover`}}>
+        <div className="style-image" style={{background: `url("https://ella-ksenofontova.github.io/interior-design-styles/Materials/Images/${styleName}.jpg") center / cover, url("Materials/Images/${styleName}.svg") center / cover, #F3DAB3 center / cover`}}>
             <div className="style-name"><h2>{styleTitle}</h2></div>
         </div>
     );
