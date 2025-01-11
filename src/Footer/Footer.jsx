ee3155f1dcbc6d999af8af5d270d25d5abefc7d7
Ella Ksenofontova <ellaksenofontova@gmail.com>
2025-01-11 20:00:43 +0300

    Добавила автоопределение года в футере и чуть-чуть отредактировала CSS стрелок карусели

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer>
-      <div className="footer-text">2023-2024 ©<a href="https://ella-ksenofontova.github.io/">Элла Ксенофонтова</a></div>
+      <div className="footer-text">2023-{new Date().getFullYear()} ©<a href="https://ella-ksenofontova.github.io/">Элла Ксенофонтова</a></div>
     </footer>
   )
 }
